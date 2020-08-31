c6376e3885ab539beeb135f16bfbf2a33d261e97
KokoTa <584847514@qq.com>
2020-08-31 14:20:42 +0800

    feat: IOS启动图

--- js/MainApp/App.js
+++ js/MainApp/App.js
@@ -1,17 +1,28 @@
 /*
  * @Author: KokoTa
  * @Date: 2020-08-10 17:30:16
- * @LastEditTime: 2020-08-20 16:30:04
+ * @LastEditTime: 2020-08-31 14:18:48
  * @LastEditors: KokoTa
  * @Description:
  * @FilePath: /AwesomeProject/js/MainApp/App.js
  */
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import AppNavigator from './navigator/AppNavigator';
 import store from './store';
+import RNBootSplash from 'react-native-bootsplash';
 
 export default function App() {
+  let init = async () => {
+    // …do multiple async tasks
+  };
+
+  useEffect(() => {
+    init().finally(() => {
+      RNBootSplash.hide({duration: 250});
+    });
+  }, []);
+
   return (
     <Provider store={store}>
       <AppNavigator />
